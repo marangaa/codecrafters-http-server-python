1f010c5bc6f79ee6478c5e0ae719da79303b5a63
marangaa <rchdmaranga@gmail.com>
2024-03-25 12:51:51 +0300

    respond to client

--- app/main.py
+++ app/main.py
@@ -7,9 +7,19 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    client_socket, address = server_socket.accept()
+
+    data = client_socket.recv(1024)
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+
+    client_socket.send(response)
+
+    client_socket.close()
+
+
 
 
 if __name__ == "__main__":
