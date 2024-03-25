766e61ab2c4b4eb3c1c49866fd6db7cb2ea41d87
marangaa <rchdmaranga@gmail.com>
2024-03-25 13:26:07 +0300

    respond with 404

--- app/main.py
+++ app/main.py
@@ -2,26 +2,15 @@ import socket
 
 
 def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    client_socket, address = server_socket.accept()
-
-    data = client_socket.recv(1024)
-
-    request_str = data.decode("utf-8")
-    request_lines = request_str.split("\r\n")
-
-    request_line = request_lines[0]
-    method, path, _ = request_line.split()
-
+    client_socket_object, address_info_port = server_socket.accept()  # wait for client
+    data = client_socket_object.recv(1024).decode("utf-8")
+    separate_lines = data.split("\r\n")
+    path = separate_lines[0].split()[1]
     if path == "/":
-        response = f"HTTP/1.1 200 OK\r\n"
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
     else:
-        response = f"HTTP/1.1 404 Not Found\r\n"
-
-    client_socket.send(response)
-    client_socket.close()
-
-
-if __name__ == "__main__":
-    main()
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    client_socket_object.send(response)
